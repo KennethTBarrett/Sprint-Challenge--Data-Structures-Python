2ba0b4757bf399087bc3ef3e02ab698d409520dc
Kenneth T. Barrett <kennethtbarrett@criptext.com>
2020-07-17 11:06:57 -0500

    Done!

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,7 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -39,4 +40,20 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        pass
+        if node is not None:  # If the node exists...
+            next = node.next_node  # Store `next` as our next node.
+            # This is where we're gonna prepare to go backwards.
+            node.next_node = prev  # Set the next node to `prev` one.
+            # Set `prev` to be our node...
+            prev = node
+            # ... and `node` to be `next` (we're shifting backwards).
+            node = next
+            self.reverse_list(node, prev)  # Recursion is your friend.
+        else:  # If the node doesn't exist...
+            self.head = prev  # Make the head our `prev` value.
+
+        # Okay, this was pretty tricky, but spending such an immense amount
+        # of time trying to understand the fundamentals behind linked lists
+        # in combination with UPER DEFINITELY made this doable, whereas I'm
+        # not sure I would have been able to do this if I didn't - cheers to
+        # putting in the time and effort! :D (also, to drawing on paper xD)
